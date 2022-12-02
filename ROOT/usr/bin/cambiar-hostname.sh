5d38f9e83aeb1c11175d3cb359f976bc755ab82d
alvaro@a33 <alvaro@a33>
2022-12-02 08:14:17 +0100

    mejor cambiar-nombre.sh

--- ROOT/usr/bin/cambiar-hostname.sh
+++ ROOT/usr/bin/cambiar-hostname.sh
@@ -4,24 +4,35 @@ set -o pipefail
 set -u
 set -e
 
-if [ "${1:-}" == "" ] ; then
-	echo 'Missing argument: new_hostname'
-	exit 1
+
+cambiar_hostname(){
+    local NEW_HOSTNAME=$1
+    local CUR_HOSTNAME=$(cat /etc/hostname)
+
+    # Change the hostname
+    sudo hostnamectl set-hostname $NEW_HOSTNAME
+    sudo hostname $NEW_HOSTNAME
+
+    # Change hostname in /etc/hosts & /etc/hostname
+    sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+    sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
+
+    # Display new hostname
+    echo "The new hostname is $NEW_HOSTNAME"
+}
+
+
+if [ "${1:-}" == "" ]
+then
+    CUR_HOSTNAME=$(cat /etc/hostname)
+    NEW_HOSTNAME=$(whiptail --inputbox "Nuevo nombre de equipo (ahora es $CUR_HOSTNAME):" 10 30 "$CUR_HOSTNAME" 3>&1 1>&2 2>&3)
+    if [ $? -gt 0 ]
+    then
+        echo "Cancelado (aquí no debería llegar con set -e)"
+        exit 1
+    fi
+else
+    NEW_HOSTNAME=$1
 fi
 
-CUR_HOSTNAME=$(cat /etc/hostname)
-NEW_HOSTNAME=$1
-
-# Display the current hostname
-echo "The current hostname is $CUR_HOSTNAME"
-
-# Change the hostname
-sudo hostnamectl set-hostname $NEW_HOSTNAME
-sudo hostname $NEW_HOSTNAME
-
-# Change hostname in /etc/hosts & /etc/hostname
-sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
-sudo sed -i "s/$CUR_HOSTNAME/$NEW_HOSTNAME/g" /etc/hostname
-
-# Display new hostname
-echo "The new hostname is $NEW_HOSTNAME"
+cambiar_hostname $NEW_HOSTNAME
