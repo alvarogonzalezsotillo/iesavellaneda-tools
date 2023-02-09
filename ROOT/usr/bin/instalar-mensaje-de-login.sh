ef64e2f82c2eacedd8e32e8d89a357baf2d2f3f8
alvaro@a33 <alvaro@a33>
2023-02-09 11:55:51 +0100

    Se pone mensaje de login y logo.

--- ROOT/usr/bin/instalar-mensaje-de-login.sh
+++ ROOT/usr/bin/instalar-mensaje-de-login.sh
@@ -15,14 +15,15 @@ check_logo_installed(){
 }
 
 uninstall_logo(){
-    sed -i '/\[org\/gnome\/login-screen\]/d' "$GREETERCONF"
-    sed -i '/^logo=/d' "$GREETERCONF"
-    sed -i '/^banner-message-text=/d' "$GREETERCONF"
-    sed -i '/^banner-message-enable=/d' "$GREETERCONF"
+    crudini --verbose --del "$GREETERCONF" "org/gnome/login-screen" "banner-message-enable"
+    crudini --verbose --del "$GREETERCONF" "org/gnome/login-screen" "banner-message-text"
+    crudini --verbose --del "$GREETERCONF" "org/gnome/login-screen" "logo"
 }
 
 install_logo(){
-    printf "%s\n" "$BANNER" >> "$GREETERCONF"
+    crudini --verbose --set "$GREETERCONF" "org/gnome/login-screen" "banner-message-enable" "true"
+    crudini --verbose --set "$GREETERCONF" "org/gnome/login-screen" "banner-message-text" 'Este ordenador se pone disposición de los alumnos por parte del IES Alonso de Avellaneda. Está sujeto a las condiciones de uso generales del aula y particulares de cada clase y profesor. Su uso está destinado exclusivamente a tareas académicas.\n\nCon el fin de garantizar su uso correcto y conservación, el personal del instituto podrá monitorizar las actividades realizadas en este ordenador, así como acceder a la información guardada y modificarla en caso necesario.'
+    crudini --verbose --set "$GREETERCONF" "org/gnome/login-screen" "logo" "/usr/share/pixmaps/logo-iesavellaneda.png"
 }
 
 if [ "$1" = "-u" ]
@@ -41,7 +42,6 @@ if check_logo_installed
 then
     echo "El logotipo ya está instalado"
 else
-    uninstall_logo
     if install_logo
     then
         echo "Logotipo instalado en $GREETERCONF"
